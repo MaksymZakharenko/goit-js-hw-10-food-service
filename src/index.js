b6ee9317c4d8da0eabbe2012d81ba770ccafcdba
MaksymZakharenko <raspik83@gmail.com>
2021-05-07 17:09:01 +0300

    test

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './styles.css';
+console.log("HI")
+
+import "./JS/test"
+
+import {text, printText} from "./JS/test"
+
+// import print from "./JS/test" дефолтный
+console.log(text);
+printText("Hello World");
+
+print("NOT HI")
