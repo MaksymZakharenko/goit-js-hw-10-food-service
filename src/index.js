ba9bfbcaa41adb0a386fbd76927724153c65d045
MaksymZakharenko <raspik83@gmail.com>
2021-05-12 18:12:22 +0300

    final 10 HM

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 import './styles.css';
-console.log("HI")
+import './JS/menu';
 
-import "./JS/test"
+const switcher = document.querySelector('#theme-switch-toggle');
 
-import {text, printText} from "./JS/test"
+switcher.addEventListener('change', changeColor);
 
-// import print from "./JS/test" дефолтный
-console.log(text);
-printText("Hello World");
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
 
-print("NOT HI")
+function changeColor(event) {
+  if (event.target.checked) {
+    currentTheme(Theme.LIGHT, Theme.DARK);
+    updateFonecolor('Theme', Theme.DARK);
+  } else {
+    currentTheme(Theme.DARK, Theme.LIGHT);
+    updateFonecolor('Theme', Theme.LIGHT);
+  }
+}
+
+function currentTheme(firstTheme, secondTheme) {
+  document.body.classList.remove(firstTheme);
+  document.body.classList.add(secondTheme);
+}
+
+function updateFonecolor(key, value) {
+  localStorage.setItem(key, value);
+}
+
+function startTheme() {
+  const theme = localStorage.getItem('Theme') ?? Theme.LIGHT;
+  document.body.classList.add(theme);
+  switcher.checked = theme === Theme.DARK;
+}
+
+startTheme();
